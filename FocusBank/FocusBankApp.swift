90e35e7cb7e6bc1c2e0b1ed079130c7b0d8f637c
kunnaalsoni <sonee069@gmail.com>
2026-06-28 23:39:00 +0530

    Initial Commit

---
+++ FocusBank/FocusBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBankApp.swift
+//  FocusBank
+//
+//  Created by Kunal Soni on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
